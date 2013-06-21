cf3b34827652f173c10b15a84790fa5502a35cd5
Anthony Short <antshort+github@gmail.com>
2013-06-22 01:04:29 +1000

    First

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var blog = require('./lib/blog');
+var content = require('./lib/content');
+var errors = require('./lib/errors');
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use('/public', express.static('public'));
+app.use('/articles', express.static('lib/blog/articles'));
+
+app.use(blog({
+  articles: './articles'
+}));
+app.use(content);
+app.use(errors);
+
+app.listen(3000);
+console.log("Listening on port 3000");
